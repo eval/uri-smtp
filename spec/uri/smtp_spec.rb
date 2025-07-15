8d6fb1cfadf606c5ab17ca6789cbd08d3897551b
Gert Goet <gert@thinkcreate.dk>
2025-07-15 22:27:13 +0200

    Fresh from `bundle gem uri-smtp`

---
+++ spec/uri/smtp_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Uri::Smtp do
+  it "has a version number" do
+    expect(Uri::Smtp::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
